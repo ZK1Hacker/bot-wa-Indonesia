8ed7e19736590835e8fab5088e2a688c263f7846
ZK1Hacker <47277281+ZK1Hacker@users.noreply.github.com>
2021-03-07 21:00:54 +0700

    Update okick.js

--- plugins/okick.js
+++ plugins/okick.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['kick','-'].map(v => 'o' + v + ' @user' + ' @user' + ' @user')
-handler.tags = ['owner']
+handler.help = ['kick','-'].map(v => 'o' + v + ' @user %s %2$d' + ' @user' + ' @user')
+handler.tags = ['owner','member','%s']
 handler.command = /^(okick|o\-)$/i
 handler.owner = true
 handler.mods = false
